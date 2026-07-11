ed6f0f9f66fc1736c08559356959d563a1c677ea
Matheus Felipe <theusf40@hotmail.com>
2026-07-11 15:04:36 -0300

    ui: added missing sidebar close button

--- resources/js/components/layout/AppLayout.vue
+++ resources/js/components/layout/AppLayout.vue
@@ -6,20 +6,27 @@ import AppSidebar from '@/components/AppSidebar.vue'
 import { Moon, Bell, SearchIcon } from 'lucide-vue-next';
 import InputGroupInput from '../ui/input-group/InputGroupInput.vue';
 import InputGroupAddon from '../ui/input-group/InputGroupAddon.vue';
+import SidebarTrigger from '../ui/sidebar/SidebarTrigger.vue';
 </script>
 
 <template>
   <SidebarProvider>
     <AppSidebar />
     <SidebarInset class="min-w-0">
-      <header class="border-b px-6 py-3 flex items-center justify-between">
-        <InputGroup class="w-full max-w-sm">
-          <InputGroupInput placeholder="Search resources..." />
-          <InputGroupAddon >
-            <SearchIcon />
-          </InputGroupAddon>
-        </InputGroup>
-        <div class="flex items-center gap-3">
+      <header class="border-b px-6 py-3 flex items-center justify-between gap-4">
+
+        <div class="flex items-center gap-3 w-full max-w-sm">
+          <SidebarTrigger class="-ml-1" />
+
+          <InputGroup class="w-full">
+            <InputGroupInput placeholder="Search resources..." />
+            <InputGroupAddon >
+              <SearchIcon />
+            </InputGroupAddon>
+          </InputGroup>
+        </div>
+
+        <div class="flex items-center gap-3 shrink-0">
           <Button variant="ghost" size="icon"><Moon class="w-4 h-4" /></Button>
           <Button variant="ghost" size="icon"><Bell class="w-4 h-4" /></Button>
         </div>
